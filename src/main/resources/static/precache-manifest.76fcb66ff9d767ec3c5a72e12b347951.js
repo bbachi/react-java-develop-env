9ad342dcef8bca1b7d017bac8412f62707c4ec52
bbachi <bhargavbachina@yahoo.com>
2020-06-28 15:50:23 -0500

    initial commit

---
+++ src/main/resources/static/precache-manifest.76fcb66ff9d767ec3c5a72e12b347951.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "629f8922bbdbb4a51babcc839b513dde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5009b8f8302e304bd12f",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "867fa92dc1eb485ee3ed",
+    "url": "/static/css/main.14ff7981.chunk.css"
+  },
+  {
+    "revision": "5009b8f8302e304bd12f",
+    "url": "/static/js/2.28dc267f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.28dc267f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "867fa92dc1eb485ee3ed",
+    "url": "/static/js/main.7734323f.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
---
+++ src/main/ui/build/precache-manifest.0c49f6913db231fdc5af2d9bf62f7211.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9212e9747ba3da1a2b6700eb5641b88c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5009b8f8302e304bd12f",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cd664973daef0f2bffeb",
+    "url": "/static/css/main.14ff7981.chunk.css"
+  },
+  {
+    "revision": "5009b8f8302e304bd12f",
+    "url": "/static/js/2.28dc267f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.28dc267f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd664973daef0f2bffeb",
+    "url": "/static/js/main.82171686.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
